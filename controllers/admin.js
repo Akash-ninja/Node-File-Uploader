050c2e78e59afb7d9b8490f76822e758456a15c8
Akash-ninja <akash.gitcode@gmail.com>
2023-02-23 13:01:18 +0530

    Handled File upload with Multer package

--- controllers/admin.js
+++ controllers/admin.js
@@ -15,9 +15,10 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
+  const imageUrl = req.file;
   const price = req.body.price;
   const description = req.body.description;
+  console.log(imageUrl);
 
   // Catching Validation errors
   const errors = validationResult(req);
--- routes/auth.js
+++ routes/auth.js
@@ -17,12 +17,6 @@ router.post(
       .isEmail()
       .withMessage("Please enter a valid email")
       .custom((value, { req }) => {
-        // custom validator
-        /* if (value === "test@test.com") {
-          throw new Error("This email address is forbidden");
-        }
-        return true; */
-
         return User.findOne({ email: value }).then((userDoc) => {
           if (userDoc) {
             return Promise.reject(

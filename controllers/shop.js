1552f25a102deb9b7ff864b5c3a3d7d59f2c3a58
Akash-ninja <akash.gitcode@gmail.com>
2023-02-25 16:08:23 +0530

    Downloading file while checking for authenticated and valid user

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,3 +1,6 @@
+const fs = require('fs')
+const path = require('path');
+
 const Product = require("../models/product");
 const Order = require("../models/order");
 
@@ -115,4 +118,32 @@ exports.getOrders = (req, res, next) => {
       });
     })
     .catch((err) => console.log(err));
+};
+
+exports.getInvoice = (req, res, next) => {
+  const orderId = req.params.orderId;
+
+  Order.findById(orderId)
+    .then((order) => {
+      if (!order) {
+        return next(new Error("No Order Found!"));
+      }
+      if (order.user.userId.toString() !== req.user._id.toString()) {
+        return next(new Error("Not Authorized"));
+      }
+      const invoiceName = "invoice-" + orderId + ".pdf";
+      const invoicePath = path.join("data", "invoices", invoiceName);
+      fs.readFile(invoicePath, (err, data) => {
+        if (err) {
+          return next(err);
+        }
+        res.setHeader("Content-Type", "application/pdf");
+        res.setHeader(
+          "Content-Disposition",
+          "inline; fileName=" + invoiceName + ""
+        );
+        res.send(data);
+      });
+    })
+    .catch((err) => next(err));
 };

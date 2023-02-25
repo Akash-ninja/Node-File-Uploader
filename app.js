a103e76bc9b8d06c5ef2abe6b3a95d14cced1917
Akash-ninja <akash.gitcode@gmail.com>
2023-02-25 12:51:35 +0530

    Serving Images Statically

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ const fileFilter = (req, file, cb) => {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer({storage: fileStorage, fileFilter}).single('image'));
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use(
   session({

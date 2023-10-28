428e37bc7f76e7316688c7b9e9038231ce7692c1
Akash-ninja <akash.gitcode@gmail.com>
2023-10-28 13:38:12 +0530

    Added image picker functionality in Reactjs file

--- App-v2/client/src/components/blogs/BlogFormReview.js
+++ App-v2/client/src/components/blogs/BlogFormReview.js
@@ -7,6 +7,8 @@ import { withRouter } from 'react-router-dom';
 import * as actions from '../../actions';
 
 class BlogFormReview extends Component {
+  state = { file: null };
+
   renderFields() {
     const { formValues } = this.props;
 
@@ -47,11 +49,23 @@ class BlogFormReview extends Component {
     submitBlog(formValues, history);
   }
 
+  onFileChange(event) {
+    this.setState({file: event.target.files[0]})
+    console.log(event.target.files[0])
+  }
+
   render() {
     return (
       <form onSubmit={this.onSubmit.bind(this)}>
         <h5>Please confirm your entries</h5>
         {this.renderFields()}
+
+        <h5>Add an Image</h5>
+        <input
+          type='file'
+          accept='image/*'
+          onChange={this.onFileChange.bind(this)}
+        />
 
         {this.renderButtons()}
       </form>
